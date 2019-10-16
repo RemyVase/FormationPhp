057a9bad6d4aa39861d50b0031060e4001495d15
RemyVase <remy.vase@hotmail.com>
2019-10-16 16:01:32 +0200

    Update

--- variable.php
+++ variable.php
@@ -5,3 +5,31 @@
  * Date: 2019-10-16
  * Time: 15:29
  */
+
+
+$age_du_visiteur = 17;
+$text = "L'âge de Patrick est : ";
+$testBool = false;
+$testBool2 = true;
+$testFloat = 12.3;
+
+
+//echo $text . $age_du_visiteur . "\n";
+//echo "L'âge de Marc est : " . $testFloat . " ans";
+
+
+
+
+$nombre = 2;
+$nombre1 = 5;
+$nombre2 = 15;
+$nombre3 = 12;
+
+$nombre4 = 12/4;
+$nombre5 = 15*3;
+$nombre6 = $nombre1 - $nombre;
+$nombre7 = $nombre2 + $nombre3;
+
+
+echo $nombre7;
+
