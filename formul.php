63841a5c51727072651ab59fc1e713ca72c020f4
RemyVase <remy.vase@hotmail.com>
2019-10-20 21:41:22 +0200

    Correction du TP et ajout commentaires

--- formul.php
+++ formul.php
@@ -1,5 +1,6 @@
 <?php
-    if(!isset($_POST['password']) OR htmlspecialchars($_POST['password']) != 'kangourou'){
+//Affichage du formulaire de base.
+    if(!isset($_POST['password'])){
         echo '<p> Entrez le mot de passe pour accéder à la page secrete ! <br> 
                 <form action="formul.php" method = "post">
                     <input type="password" name="password">
@@ -7,6 +8,17 @@
                 </form>
             </p>';
     }
+    //Affichage si le mot de passe est incorrect.
+    elseif(htmlspecialchars($_POST['password']) != 'kangourou'){
+        echo '<p> Entrez le mot de passe pour accéder à la page secrete ! <br> 
+                <form action="formul.php" method = "post">
+                    <input type="password" name="password">
+                    <input type="submit" name="valider">
+                </form>
+            </p><br>
+            <p>MOT DE PASSE INCORRECT</p>';
+    }
+    //Affichage si le mot de passe est correct.
     else{
         echo 'Bienvenue dans la chambre des secrets Harry, ici tu affronteras celui dont nul ne peut prononcer le nom !';
     }
